f6df0c41b7211c25b0a202746880cecb81f29d27
Drew Fleeman <drew.fleeman@gmail.com>
2017-06-26 13:33:37 -0500

    open sign up page with safari

--- onTheMap/LoginViewController.swift
+++ onTheMap/LoginViewController.swift
@@ -9,14 +9,30 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
+    @IBAction func openUdacitySignUpOnSafari(_ sender: UIButton) {
+        if let requestUrl = URL(string: "https://www.udacity.com/account/auth#!/signup") {
+            UIApplication.shared.open(requestUrl, options: [:], completionHandler: nil)
+        }
+    }
     
-
+    @IBAction func authenticateCredentials(_ sender: UIButton) {
+        
+        // send email and pword to udacity api to authenticate
+        
+        // if successful: segue to map
+        
+        // if unsuccessful: alert view w/ either failed network or incorrect credentials notice
+        
+    }
 
 }
 
